c5c2fe0169d92894d7a440f32edbb30d3ce2fe15
GeorgeGE7 <elgeo.e.ge.7@gmail.com>
2024-01-08 22:17:59 +0200

    Done with base model and its testing

---
+++ models/base_model.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""
+Module: base_model.py
+This is the "base model" module.
+"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """BaseModel is the parent for other classes
+
+    Attributes:
+      id(str): the unique id of the object(user)
+      created_at: created at time
+      updated_at: update at time
+
+    Methods:
+      __str__: return string
+      save(self): change updated_at atr
+      to_dict(self): returns the keys and values of the dict
+
+    """
+    def __init__(self, *args, **kwargs):
+        """Object init
+
+        Args:
+          args: won't used
+          kwargs: a existing dictionary
+
+        """
+
+        string_time_formate = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, string_time_formate)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            
+
+
+    def save(self):
+        """
+        Changing the updated_at prop:
+        with new current one
+        """
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """
+        Method returns a dict with keys 
+        and values of the dictionary
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+    
+    def __str__(self):
+        """
+        Print a string of the obj
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    
+# To be deleted - not prisite
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+Unit testing for the base_model
+"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Testing the base model
+    """
+
+    def test_initialization(self):
+        """
+        Testing initialization of BaseModel
+        """
+        my_model = BaseModel()
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.updated_at)
+
+    def test_save(self):
+        """
+        Test the save() if update time correctly
+        """
+        my_model = BaseModel()
+        my_model.save()
+        self.assertNotEqual(my_model.created_at, my_model.updated_at)
+
+    def test_to_dict(self):
+        """
+        Test the to_dict method if only returns just dictionary
+        """
+        my_model = BaseModel()
+        my_model_dict = my_model.to_dict()
+
+        self.assertIsInstance(my_model_dict, dict)
+
+        self.assertEqual(my_model_dict["__class__"], "BaseModel")
+        self.assertEqual(my_model_dict["id"], my_model.id)
+        self.assertEqual(my_model_dict["created_at"], my_model.created_at.isoformat())
+        self.assertEqual(my_model_dict["updated_at"], my_model.updated_at.isoformat())
+
+    def test_str(self):
+        """
+        Test the __str__ if it returns just a string
+        """
+        my_model = BaseModel()
+
+        self.assertTrue(str(my_model).startswith('[BaseModel]'))
+        self.assertIn(my_model.id, str(my_model))
+        self.assertIn(str(my_model.__dict__), str(my_model))
+
+    def test_kwargs(self):
+        """
+        Test the constractor with kwargs
+        """
+        my_model = BaseModel(created_at="2019-07-01T00:00:00.000000", updated_at="2019-07-01T00:00:00.000000")
+        self.assertEqual(my_model.created_at, datetime.fromisoformat("2019-07-01T00:00:00.000000"))
+        self.assertEqual(my_model.updated_at, datetime.fromisoformat("2019-07-01T00:00:00.000000"))
+        self.assertNotEqual(my_model.created_at, datetime.utcnow())
+        self.assertNotEqual(my_model.updated_at, datetime.utcnow())
+
+if __name__ == "__main__":
+  unittest.main()
