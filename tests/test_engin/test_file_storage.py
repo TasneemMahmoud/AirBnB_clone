d901c07e32cd1eff1a12a293d87efdfd804f5ca6
GeorgeGE7 <elgeo.e.ge.7@gmail.com>
2024-01-13 16:01:41 +0200

    Nothing

--- tests/test_engin/test_file_storage.py
+++ tests/test_engin/test_file_storage.py
@@ -15,21 +15,27 @@ from models.engine.file_storage import FileStorage
 class TestFileStorage(unittest.TestCase):
     def test_init(self):
         self.assertEqual(type(FileStorage()), FileStorage)
+
     def test_method_all(self):
         self.assertEqual(dict, type(models.storage.all()))
+
     def test_type_of_file(self):
         self.assertEqual(str, type(FileStorage()._FileStorage__file_path))
+
     def test_storage_type(self):
         self.assertEqual(type(models.storage), FileStorage)
+
     def test_method_new(self):
         base = BaseModel()
         models.storage.new(base)
         self.assertIn("BaseModel." + base.id, models.storage.all().keys())
         self.assertIn(base, models.storage.all().values())
+
     def test_correct_obj_cer(self):
         obj = BaseModel()
         models.storage.new(obj)
         self.assertIn(obj, models.storage.all().values())
+
     def test_more_inst(self):
         obj1 = BaseModel()
         obj2 = BaseModel()
@@ -37,6 +43,7 @@ class TestFileStorage(unittest.TestCase):
         models.storage.new(obj2)
         self.assertIn(obj1, models.storage.all().values())
         self.assertIn(obj2, models.storage.all().values())
+
     def test_method_save(self):
         base = BaseModel()
         models.storage.new(base)
@@ -47,6 +54,7 @@ class TestFileStorage(unittest.TestCase):
             self.assertIn("BaseModel." + base.id, save_text)
         with self.assertRaises(TypeError):
             models.storage.save(None)
+
     def test_method_reload(self):
         base = BaseModel()
         models.storage.new(base)
